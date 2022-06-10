33884a75640bbc9f12d087caa9e8ed349120bf09
jahanzab sabir <you@example.com>
2022-06-10 18:17:28 +0500

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+  <script src="asset/js/jquery.min.js"></script>
+  <script src="asset/js/jquery.easing.min.js"></script>
+  <script src="asset/js/bootstrap.min.js"></script>
+  <script src="asset/js/custom.js"></script>
+</body>
+</html>
